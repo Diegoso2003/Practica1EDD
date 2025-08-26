1e66c061b9f147f22436a3f1175a81412fb02df2
Diego <diegocayax202231074@cunoc.edu.gt>
2025-08-26 17:50:37 -0600

    creada la clase encargada de confirmar creacion de cuadros

---
+++ Juego/VerificadorCuadros/VerificadorCuadros.h
@@ -0,0 +1,13 @@
+//
+// Created by rafael-cayax on 26/8/25.
+//
+
+#ifndef PRACTICA1EDD_VERIFICADORCUADROS_H
+#define PRACTICA1EDD_VERIFICADORCUADROS_H
+
+
+class VerificadorCuadros {
+};
+
+
+#endif //PRACTICA1EDD_VERIFICADORCUADROS_H
